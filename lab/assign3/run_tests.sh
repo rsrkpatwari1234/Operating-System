2613aa91519c4b097a39e17ce38a0ccbd1103183
atharva naik <atharvanaik2018@gmail.com>
2021-02-11 01:03:26 +0530

    shell script for running make tests

---
+++ lab/assign3/run_tests.sh
@@ -0,0 +1,5 @@
+make tests/threads/alarm-zero.result
+make tests/threads/alarm-negative.result
+make tests/threads/alarm-single.result
+make tests/threads/alarm-multiple.result
+make tests/threads/alarm-simultaneous.result
