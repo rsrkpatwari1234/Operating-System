3e58ccdc2e8291a6af31ff78c3f9227186d8f59b
radhika patwari <rsrkpatwari1234@gmail.com>
2021-03-24 16:52:23 +0530

    syscall write done

--- lab/assign6/syscall.c
+++ lab/assign6/syscall.c
@@ -11,14 +11,15 @@
 #include "userprog/process.h"
 #include <user/syscall.h>
 
-#define MAX_ARGS 3
+/*#define MAX_ARGS 3*/
 #define STD_INPUT 0
 #define STD_OUTPUT 1
 
 void get_args (struct intr_frame *f, int *arg, int num_of_args);
-int syscall_write (int filedes, const void * buffer, unsigned byte_size);
+int syscall_write (int filedes, int buffer, int byte_size);
 void validate_ptr (const void* vaddr);
 void validate_buffer (const void* buf, unsigned byte_size);
+void* check_addr(const void*);
 
 /* Assignment 6 : 2.4 ended */
 
@@ -50,33 +51,61 @@ syscall_handler (struct intr_frame *f UNUSED)
 
   /* Assignment 6 : 2.4 started */
 
-  int arg[MAX_ARGS];
-  int esp = getpage_ptr((const void *) f->esp);
-  switch (* (int *) esp)
+  /*int arg[MAX_ARGS];
+  int esp = getpage_ptr((const void *) f->esp);*/
+
+  int* p = f->esp;
+  check_addr(p);
+  int system_call = *p;
+  printf("here %d",system_call);
+
+  switch (system_call)
   {    
     case SYS_EXIT:
       // fill arg with the amount of arguments needed
-      get_args(f, &arg[0], 1);
-      syscall_exit(arg[0]);
+      //get_args(f, &arg[0], 1);
+      check_addr(p+1);
+      syscall_exit(*(p+1));
       break;
        
     case SYS_WRITE:
       
       // fill arg with the amount of arguments needed
-      get_args(f, &arg[0], 3);
-      printf("-1-");
+      /*get_args(f, &arg[0], 3);
+      printf("-1-");*/
       /* Check if the buffer is valid.
        * We do not want to mess with a buffer that is out of our
        * reserved virtual memory 
        */
-      validate_buffer((const void*)arg[1], (unsigned)arg[2]);
-      printf("-2-");
+      /*validate_buffer((const void*)arg[1], (unsigned)arg[2]);
+      printf("-2-");*/
       // get page pointer
-      arg[1] = getpage_ptr((const void *)arg[1]); 
-      printf("-3-");
-      /* syscall_write (int filedes, const void * buffer, unsigned bytes)*/
-      f->eax = syscall_write(arg[0], (const void *) arg[1], (unsigned) arg[2]);
-      printf("-4-");
+      /*arg[1] = getpage_ptr((const void *)arg[1]); 
+      printf("-3-");*/
+      check_addr(p+7);
+      check_addr(p+6);
+      f->eax = syscall_write (*(p+5), *(p+6), *(p+7));
+      /*f->eax = syscall_write(arg[0], (const void *) arg[1], (unsigned) arg[2]);
+      printf("-4-");*/
+      /*check_addr(p+7);
+      check_addr(p+6);
+      if(*(p+5)==1)
+      {
+        putbuf(*(p+6),*(p+7));
+        f->eax = *(p+7);
+      }*/
+      /*else
+      {
+        struct proc_file* fptr = list_search(&thread_current()->files, *(p+5));
+        if(fptr==NULL)
+          f->eax=-1;
+        else
+        {
+          acquire_filesys_lock();
+          f->eax = file_write (fptr->ptr, *(p+6), *(p+7));
+          release_filesys_lock();
+        }
+      }*/
       break;
       
     default:
@@ -117,7 +146,7 @@ syscall_exit (int status)
 
 /* syscall_write */
 int 
-syscall_write (int filedes, const void * buffer, unsigned byte_size)
+syscall_write (int filedes, int buffer, int byte_size)
 {
     if (byte_size <= 0)
     {
@@ -169,5 +198,20 @@ getpage_ptr(const void *vaddr)
   return (int)ptr;
 }
 
+void* check_addr(const void *vaddr)
+{
+  if (!is_user_vaddr(vaddr))
+  {
+    syscall_exit(ERROR);
+    return 0;
+  }
+  void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
+  if (!ptr)
+  {
+    syscall_exit(ERROR);
+    return 0;
+  }
+  return ptr;
+}
 
 /* Assignment 6 : 2.4 ended */
